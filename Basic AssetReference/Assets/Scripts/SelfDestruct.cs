5c74ebd3db31d30df71c4c8f63760bbd7fb05df5
dhmo <modone@qq.com>
2024-08-09 17:07:02 +0800

    提交测试工程

---
+++ Basic AssetReference/Assets/Scripts/SelfDestruct.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class SelfDestruct : MonoBehaviour {
+
+	public float lifetime = 2f;
+
+	void Start()
+	{
+		Invoke("Release", lifetime);
+	}
+
+	void Release()
+	{
+        if (!Addressables.ReleaseInstance(gameObject))
+            Destroy(gameObject);
+	}
+}
